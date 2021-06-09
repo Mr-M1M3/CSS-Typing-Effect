6275be01f0c2758d0b208adfa14f48b9d03abe9f
Abir Sheikh <abir.sheikh.4044@gmail.com>
2021-06-09 19:10:55 +0600

    Added blink effect

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+var elem = document.getElementById("text");
+var elem_content = elem.innerHTML;
+function text() {
+  return elem.innerHTML;
+}
+var index = 0
+elem.innerHTML = "";
+setInterval(() => {
+  elem.innerHTML += elem_content.charAt(index);
+  index++;
+  if (index == elem_content.length) {
+    setTimeout(() => {
+      elem.innerHTML = "";
+      index = 0;
+    }, 1000);
+  }
+}, 200);
