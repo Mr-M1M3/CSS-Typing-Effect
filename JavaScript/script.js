86ee66cb584be0bcd00329f81f68dc29da8dbd01
Abir Sheikh <abir.sheikh.4044@gmail.com>
2021-06-10 09:15:05 +0600

    Dynamic typing added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,18 +1,42 @@
-// Created by Abir Sheikh
-var elem = document.getElementById("text");
-var elem_content = elem.innerHTML;
-function text() {
-  return elem.innerHTML;
-}
-var index = 0
-elem.innerHTML = "";
-setInterval(() => {
-  elem.innerHTML += elem_content.charAt(index);
-  index++;
-  if (index == elem_content.length) {
-    setTimeout(() => {
-      elem.innerHTML = "";
-      index = 0;
-    }, 1000);
+function type() {
+  var texts_type = ["Abir Sheikh", "A Human", "A Web Developer"];
+  var arr_index = 0;
+  let index = 0;
+  let typing_speed = 200;
+  const type_container = document.getElementById("typewriter-container");
+  let current_text_to_type = texts_type[arr_index];
+  let text_container = document.getElementById("text-to-type");
+  text_container.innerHTML = "";
+  type_forward();
+  function type_forward() {
+    var typing_forward = setInterval(() => {
+      text_container.innerHTML += current_text_to_type.charAt(index);
+      index++;
+      if (index == current_text_to_type.length) {
+        index = 0;
+        clearInterval(typing_forward);
+        setTimeout(() => {
+          arr_index++;
+          if (arr_index == texts_type.length) {
+            arr_index = 0;
+          }
+          type_backward();
+        }, 2000);
+      }
+    }, typing_speed);
   }
-}, 200);
+  function type_backward() {
+    var typing_backward = setInterval(() => {
+      text_container.innerHTML = text_container.innerHTML.substr(
+        0,
+        text_container.innerHTML.length - 1
+      );
+      if (text_container.innerHTML.length == 0) {
+        clearInterval(typing_backward);
+        current_text_to_type = texts_type[arr_index];
+        type_forward();
+      }
+    }, typing_speed);
+  }
+}
+type();
